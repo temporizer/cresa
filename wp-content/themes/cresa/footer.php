37fe71776051ab5a02fd37ac15252482c2d447c8
temporizer <temporizer@gmail.com>
2014-06-08 23:06:51 -0700

    modifying template

--- wp-content/themes/cresa/footer.php
+++ wp-content/themes/cresa/footer.php
@@ -7,25 +7,33 @@
 
 
 <footer>
+				<div class="search">	
+					<label for="search">Search:</label> <input type="text" name="search" id="search" size="16">
+				</div>
 				
-				
-<!--				<div id="social">
-					<a href="mobileapp.html" title="mobile apps" id="mobile"><img src="images/phone.png" alt="mobile phone icon by Jino & Christine"></a>
+				<div id="social">
+					<a href="mobileapp.html" title="mobile apps" id="mobile"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/phone.png" alt="mobile phone icon by Jino & Christine"></a>
 
-					<a href="http://www.cresa911.blogspot.com/" title="Blog" id="blog"><img src="images/blogspot.png" alt="blog icon "></a> <!-- credit by www.alessandracravero.it -->
-<!--					<a href="https://www.facebook.com/CRESA.911" title="Facebook" id="facebook"><img src="images/fb.png" alt="facebook logo by www.alessandracravero.it"></a>
-					<a href="https://twitter.com/cresa" title="Twitter icon By www.alessandracravero.it" id="twitter"><img src="images/twitter.png" alt="twitter logo by www.alessandracravero.it"></a>
+					<a href="http://www.cresa911.blogspot.com/" title="Blog" id="blog"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/blogspot.png" alt="blog icon "></a> <!-- credit by www.alessandracravero.it -->
+					<a href="https://www.facebook.com/CRESA.911" title="Facebook" id="facebook"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/fb.png" alt="facebook logo by www.alessandracravero.it"></a>
+					<a href="https://twitter.com/cresa" title="Twitter icon By www.alessandracravero.it" id="twitter"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/twitter.png" alt="twitter logo by www.alessandracravero.it"></a>
 					
--->					
-<!--					<a href="http://feeds.feedburner.com/cresa911" title="rss" id="rss"><img src="images/rss.png" alt="rss logo by www.alessandracravero.it"></a>
--->
-<!--			</div> <!-- end social --> 
-			
+					
+					<a href="http://feeds.feedburner.com/cresa911" title="rss" id="rss"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/rss.png" alt="rss logo by www.alessandracravero.it"></a>
+				</div> <!-- end social -->
+				
 				<div class="container finfo">
-					<div class="row">	
-<!--						<div class="col-sm-3" id="map">
+					<div class="row">
+	                                  <div class="col-sm-3" id="location">
+							<p>710 West 13th Street</p>
+							<p>Vancouver, WA 98660-2810</p>
+							<p>Phone: (360) 737-1911</p>
+							<p>Fax: (360) 694-1954</p>
+							<p>Email: cresa@clark.wa.gov</p>
+						</div> <!-- end location -->
+
+						<div class="col-sm-3" id="map">
 						<iframe src="https://www.google.com/maps/embed?pb=!1m20!1m8!1m3!1d2789.9779632991995!2d-122.678065!3d45.631179!3m2!1i1024!2i768!4f13.1!4m9!1i0!3e6!4m0!4m5!1s0x5495a8b7b93c0ba1%3A0x7c7630132a5e7438!2sCresa+911%2C+710+W+13th+St%2C+Vancouver%2C+WA+98660!3m2!1d45.631177!2d-122.678068!5e0!3m2!1sen!2sus!4v1400653002290" width="150" height="150" frameborder="0" style="border:0"></iframe>
--->
 							<!-- <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 							<div>
 								<div id="gmap_canvas" style="height:150px;width:150px;"></div>
@@ -35,15 +43,9 @@
 							<script type="text/javascript"> function init_map(){var myOptions = {zoom:13,center:new google.maps.LatLng(45.6310753,-122.67807040000002),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(45.6310753, -122.67807040000002)});infowindow = new google.maps.InfoWindow({content:"<b>CRESA 9-1-1</b><br/>710 West 13th Street<br/>98660-2810 Vancouver, WA" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script> -->
 						</div> <!-- end map -->
 						
-						<div class="col-sm-4" id="location">
-							<p>710 West 13th Street</p>
-							<p>Vancouver, WA 98660-2810</p>
-							<p>Phone: (360) 737-1911</p>
-							<p>Fax: (360) 694-1954</p>
-							<p>Email: cresa@clark.wa.gov</p>
-						</div> <!-- end location -->
+						
 						<!-- <div class="col-sm-2"></div> -->
-						<div class="col-sm-4" id="footnav1">
+						<div class="col-sm-3" id="footnav1">
 							<ul>
 								<li><a href="records.html" title="Records Request">Records Request</a></li>
 								<li><a href="when911.html" title="When to call">When to call</a></li>							
@@ -53,7 +55,7 @@
 						
 						</div> <!-- footnav1 -->
 						
-						<div class="col-sm-4" id="footnav2">
+						<div class="col-sm-3" id="footnav2">
 							<ul>
 								<li><a href="prepared.html" title="Emergency Preparedness">Be Prepared</a></li>
 								<li><a href="getinvolved.html" title="Get Involved">Get Involved</a></li>
@@ -62,18 +64,9 @@
 							</ul>
 						</div> <!-- footnav2 -->
 					</div>
-	<!--			</div>  -->
+				</div>
 
-				<div id="social">
-					<a href="mobileapp.html" title="mobile apps" id="mobile"><img src="<?php bloginfo('stylesheet_directory') ?>/images/phone.png" alt="mobile apps"></a><!-- icon by Jino & Christine -->
-
-					<a href="http://www.cresa911.blogspot.com/" title="Blog" id="blog"><img src="<?php bloginfo('stylesheet_directory') ?>/images/blogspot.png" alt="blog"></a> <!-- credit by www.alessandracravero.it -->
-					<a href="https://www.facebook.com/CRESA.911" title="Facebook" id="facebook"><img src="<?php bloginfo('stylesheet_directory') ?>/images/fb.png" alt="facebook"></a><!-- logo by www.alessandracravero.it -->
-					<a href="https://twitter.com/cresa" title="Twitter" id="twitter"><img src="<?php bloginfo('stylesheet_directory') ?>/images/twitter.png" alt="twitter"></a><!-- logo by www.alessandracravero.it -->
-					
-					
-					<a href="http://feeds.feedburner.com/cresa911" title="rss" id="rss"><img src="<?php bloginfo('stylesheet_directory') ?>/images/rss.png" alt="rss"></a><!-- logo by www.alessandracravero.it -->
-				</div> <!-- end social -->				
+				
 				
 			</footer>
 		</div> <!-- end pagewrapper -->
@@ -93,8 +86,7 @@
 			    childrenIndenter: '&nbsp;'
 			});
 		</script>
-
-
+					
 <?php wp_footer(); ?>
 </body>
 </html>
